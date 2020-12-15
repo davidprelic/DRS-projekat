e79bba2a84b7958992afc584a03aa079130c3f4e
nikolinagavranovic <62725204+nikolinagavranovic@users.noreply.github.com>
2020-12-15 18:34:08 +0100

    Start game and first window.

---
+++ first_window.py
@@ -0,0 +1,62 @@
+from PyQt5.QtGui import QPixmap, QFont
+from PyQt5.QtWidgets import QWidget, QPushButton, QMainWindow, QHBoxLayout, \
+    QDesktopWidget, QVBoxLayout, QLabel
+
+
+
+class FirstWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle("Bubble Bobble")
+
+        self.central_widget = QWidget()
+        self.setCentralWidget(self.central_widget)
+        lay = QVBoxLayout(self.central_widget)
+
+        label = QLabel(self)
+        pixmap = QPixmap('Images/first_window_img.jpg')
+        label.setPixmap(pixmap)
+        self.resize(pixmap.width(), pixmap.height())
+
+        label.setScaledContents(True)
+
+        lay.addWidget(label)
+
+        self.start_1p_btn = QPushButton("start 1P game", self)
+        self.start_1p_btn.setGeometry(400, 280, 200, 50)
+        self.start_1p_btn.setFont(QFont('Times', 15))
+        self.start_1p_btn.setStyleSheet("background-color : yellow")
+        self.start_1p_btn.clicked.connect(self.show_game_window_1p)
+
+        self.start_2p_btn = QPushButton("start 2P game", self)
+        self.start_2p_btn.setGeometry(400, 380, 200, 50)
+        self.start_2p_btn.setFont(QFont('Times', 15))
+        self.start_2p_btn.setStyleSheet("background-color : yellow")
+        self.start_2p_btn.clicked.connect(self.show_game_window_2p)
+
+        self.quit_btn = QPushButton("quit", self)
+        self.quit_btn.setGeometry(400, 480, 200, 50)
+        self.quit_btn.setFont(QFont('Times', 15))
+        self.quit_btn.setStyleSheet("background-color : pink")
+        self.quit_btn.clicked.connect(self.quit_game)
+
+        self.center()
+        self.show()
+
+    def show_game_window_1p(self):
+        self.close()
+
+    def show_game_window_2p(self):
+        pass
+
+    def quit_game(self):
+        self.close()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
